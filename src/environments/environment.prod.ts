59fe6b48cd1627b2c5063c899b5a2bafa62ff8dd
Satish Gupta <satish.gupta@clik.ai>
2020-06-29 19:49:57 +0530

    environment variable changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  hostUrl: 'http://localhost:3000',
+  hostUrl: 'http://3.6.178.94:3000',
   AWSAccessKeyId: '',
   AWSSecretKey: '',
   AWSregion: 'ap-south-1',
