a6e11c1a744dd700bf9c61a037ac5f4c65f96fc6
Satish Gupta <satish.gupta@clik.ai>
2020-07-18 23:07:06 +0530

    Fixed capta in center

--- src/app/website/components/layout/header/header.component.ts
+++ src/app/website/components/layout/header/header.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
+const loggedInRoutes = [
+  { routePath: '/private-room', title: 'Private Room' },
+  { routePath: '/documents', title: 'Download Documents' },
+];
+const routes = [
+  { routePath: '/', title: 'Home' },
+  { routePath: '/suggestions', title: 'Suggestion' },
+  // { routePath: '/yatrayen', title: 'Yatrayen'},
+  { routePath: '/impacts', title: 'Impacts' },
+  { routePath: '/brand-bihar', title: 'Bihar Brand' },
+];
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,9 +20,12 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
+  navigationMenus = [];
+
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.navigationMenus = this.authService.isLoggedIn() ? loggedInRoutes : routes;
   }
 
 }
